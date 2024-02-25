136c7aed779829c7f4c1745d152a8270313ae2ab
SanaNazariNezhad <sana.nazari1995@gmail.com>
2024-02-25 18:31:32 +0330

    total files.

---
+++ app/build/generated/source/buildConfig/androidTest/debug/ir/barook/todonote/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ir.barook.todonote.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "ir.barook.todonote.test";
+  public static final String BUILD_TYPE = "debug";
+}
